60de9c5372abbbafb4bc467a3ff1a24b20280434
Diegoisawesome <diego@domoreaweso.me>
2018-07-26 18:25:58 -0700

    Change -d argument to enable debug handler
    Change device type default to 0 again

--- src/gbafix.c
+++ src/gbafix.c
@@ -33,7 +33,7 @@
 
 	History
 	-------
-	v1.05 - added device type argument, (Diegoisawesome)
+	v1.05 - added debug offset argument, (Diegoisawesome)
 	v1.04 - converted to plain C, (WinterMute)
 	v1.03 - header.fixed, header.device_type
 	v1.02 - redefined the options (rgbfix style), checksum=0
@@ -62,7 +62,7 @@ typedef struct
 	uint16_t	maker_code;			//
 	uint8_t		fixed;				// 0x96
 	uint8_t		unit_code;			// 0x00
-	uint8_t		device_type;		// 0x80
+	uint8_t		device_type;		// 0x00
 	uint8_t		unused[7];			//
 	uint8_t		game_version;		// 0x00
 	uint8_t		complement;			// 800000A0..800000BC
@@ -100,7 +100,7 @@ const Header good_header =
 	// unit_code
 	0x00,
 	// device type
-	0x80,
+	0x00,
 	// unused
 	{ 0x00,0x00,0x00,0x00,0x00,0x00,0x00 },
 	// game version
@@ -142,7 +142,7 @@ int main(int argc, char *argv[])
 	if (argc <= 1)
 	{
 		printf("GBA ROM fixer v"VER" by Dark Fader / BlackThunder / WinterMute / Diegoisawesome \n");
-		printf("Syntax: gbafix <rom.gba> [-p] [-t[title]] [-c<game_code>] [-m<maker_code>] [-r<version>] [-d<device_type>]\n");
+		printf("Syntax: gbafix <rom.gba> [-p] [-t[title]] [-c<game_code>] [-m<maker_code>] [-r<version>] [-d<debug>]\n");
 		printf("\n");
 		printf("parameters:\n");
 		printf("	-p              Pad to next exact power of 2. No minimum size!\n");
@@ -150,7 +150,7 @@ int main(int argc, char *argv[])
 		printf("	-c<game_code>   Patch game code (four characters)\n");
 		printf("	-m<maker_code>  Patch maker code (two characters)\n");
 		printf("	-r<version>     Patch game version (number)\n");
-		printf("	-d<device_type> Patch device type (number)\n");
+		printf("	-d<debug>       Enable debugging handler and set debug entry point (0 or 1)\n");
 		return -1;
 	}
 
@@ -173,6 +173,9 @@ int main(int argc, char *argv[])
 	fseek(infile, 0, SEEK_SET);
 	fread(&header, sizeof(header), 1, infile);
 
+	// fix some data
+	memcpy(header.logo, good_header.logo, sizeof(header.logo));
+	memcpy(&header.fixed, &good_header.fixed, sizeof(header.fixed));
 	memcpy(&header.device_type, &good_header.device_type, sizeof(header.device_type));
 
 	// parse command line
@@ -246,10 +249,11 @@ int main(int argc, char *argv[])
 					break;
 				}
 
-				case 'd':	// device type
+				case 'd':	// debug
 				{
 					if (!VALUE[0]) { printf("Need value for %s\n", ARGV); break; }
-					header.device_type = (unsigned char)NUMBER;
+					header.logo[0x9C-0x04] = 0xA5;	// debug enable
+					header.device_type = (unsigned char)((NUMBER & 1) << 7);	// debug handler entry point
 					break;
 				}
 
@@ -260,10 +264,6 @@ int main(int argc, char *argv[])
 			}
 		}
 	}
-
-	// fix some more data
-	memcpy(header.logo, good_header.logo, sizeof(header.logo));
-	memcpy(&header.fixed, &good_header.fixed, sizeof(header.fixed));
 
 	// update complement check & total checksum
 	header.complement = 0;
